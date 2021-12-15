0650a7a7e9a082852b7142d4a08cc97bb6179524
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-12-15 11:17:02 +0530

    Checking if receipe added to cookbook cli

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,7 +4,10 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
-
+      const deliciousCookBook = new Cookbook();
+      const deliciousCookBookCLI = new CookbookCli(deliciousCookBook);
+      const message = deliciousCookBookCLI.add('cake',['flour','cream','egg', 'sugar', 'salt']);
+      expect(message).toEqual('Successfully added the following recipe: cake'); 
     });
   });
 
