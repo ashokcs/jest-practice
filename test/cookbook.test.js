43097b9c471944b9b1a46ff478b5d305b38aa44f
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-12-15 11:11:06 +0530

    Retrieving specific receipe from the cookbook list

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -4,7 +4,7 @@ describe('Cookbook', () => {
   describe('Adding recipes', () => {
     test('should allow a recipe to be added', () => {
       const deliciousCookBook = new Cookbook();
-      deliciousCookBook.addRecipe("cake",['flour','cream','egg', 'sugar', 'salt']);
+      deliciousCookBook.addRecipe('cake',['flour','cream','egg', 'sugar', 'salt']);
       expect(deliciousCookBook.recipes).toEqual({cake: ['flour','cream','egg', 'sugar', 'salt']});
     });
   });
@@ -12,7 +12,7 @@ describe('Cookbook', () => {
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
       const deliciousCookBook = new Cookbook();
-      deliciousCookBook.addRecipe("cake",['flour','cream','egg', 'sugar', 'salt']);
+      deliciousCookBook.addRecipe('cake',['flour','cream','egg', 'sugar', 'salt']);
       deliciousCookBook.addRecipe('pizza',['flour','broccoli','mushroom', 'cheese', 'sauce', 'macaroni', 'flakes']);
       const receipiesList = deliciousCookBook.listRecipes();
       expect(receipiesList).toEqual(['cake','pizza']);
@@ -21,7 +21,11 @@ describe('Cookbook', () => {
 
   describe('Retrieving a recipe', () => {
     test('should allow the ingredients for a recipe to be retrieved', () => {
-
+      const deliciousCookBook = new Cookbook();
+      deliciousCookBook.addRecipe('cake',['flour','cream','egg', 'sugar', 'salt']);
+      deliciousCookBook.addRecipe('pizza',['flour','broccoli','mushroom', 'cheese', 'sauce', 'macaroni', 'flakes']);
+      const pizzaReceipe = myCookbook.getRecipe(['pizza']);
+      expect(pizzaReceipe).toEqual(['flour','broccoli','mushroom', 'cheese', 'sauce', 'macaroni', 'flakes']);
     });
   });
 
